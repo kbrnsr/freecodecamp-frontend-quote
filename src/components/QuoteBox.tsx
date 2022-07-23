ddd35816426935722801c3819e068e9b9ac32743
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-07-23 17:02:15 +0200

    Max line length 80

--- src/components/QuoteBox.tsx
+++ src/components/QuoteBox.tsx
@@ -3,11 +3,35 @@ import constants from './constants';
 
 function QuoteBox() {
   return (
-    <div className="QuoteBox" data-testid={constants.quoteBox} id={constants.quoteBox}>
-      <div className="QuoteText" data-testid={constants.quoteText} id={constants.quoteText} />
-      <div className="QuoteAuthor" data-testid={constants.quoteAuthor} id={constants.quoteAuthor} />
-      <button aria-label={constants.quoteNew} type="submit" className="QuoteNew" data-testid={constants.quoteNew} id={constants.quoteNew} />
-      <a href="index" data-testid={constants.quoteTweet} id={constants.quoteTweet}>Tweet</a>
+    <div
+      className="QuoteBox"
+      data-testid={constants.quoteBox}
+      id={constants.quoteBox}
+    >
+      <div
+        className="QuoteText"
+        data-testid={constants.quoteText}
+        id={constants.quoteText}
+      />
+      <div
+        className="QuoteAuthor"
+        data-testid={constants.quoteAuthor}
+        id={constants.quoteAuthor}
+      />
+      <button
+        aria-label={constants.quoteNew}
+        type="submit"
+        className="QuoteNew"
+        data-testid={constants.quoteNew}
+        id={constants.quoteNew}
+      />
+      <a
+        href="index"
+        data-testid={constants.quoteTweet}
+        id={constants.quoteTweet}
+      >
+        Tweet
+      </a>
     </div>
   );
 }
