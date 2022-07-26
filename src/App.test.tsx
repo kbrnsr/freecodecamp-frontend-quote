99c99874eb9e4c82bdb0eca0542e57a1e88c50d5
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-07-26 17:07:49 +0200

    Hopefully the last time I edit App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  let linkElement;
+  await act(() => {
+    linkElement = screen.getByText(/learn react/i);
+  });
   expect(linkElement).toBeInTheDocument();
 });
