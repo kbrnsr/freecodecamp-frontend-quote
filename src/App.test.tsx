e454a8d2743e7548464c476608ee2f40868682c1
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-07-26 16:32:59 +0200

    Reverting src/App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
 import App from './App';
 
-test('renders learn react link', async () => {
+test('renders learn react link', () => {
   render(<App />);
-  await act(async () => {
-    const linkElement = await screen.findByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-  });
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
