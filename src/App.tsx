78d72b99926cd26c75804838da01114d542c2d79
Kabeer Nasir <1452229+kbrnsr@users.noreply.github.com>
2022-07-28 15:11:16 +0200

    remove default boiler plate code from create-react-app 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    src/App.css 	deleted:    src/App.test.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,9 @@ import './App.css';
 import QuoteBox from './components/QuoteBox';
 
 function App() {
-  const codeString = 'Edit <code>src/App.tsx</code> and save to reload.';
   return (
     <div className="App">
       <QuoteBox />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {codeString}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
